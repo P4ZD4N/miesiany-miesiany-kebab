d5bc2a4ec8a78b7f2d2cb7b4ca4618eec3ec7623
Wiktor <wiko700@gmail.com>
2024-08-04 20:34:57 +0200

    feat: add Order entity

---
+++ backend/src/main/java/com/p4zd4n/kebab/entities/Order.java
@@ -0,0 +1,84 @@
+package com.p4zd4n.kebab.entities;
+
+import com.p4zd4n.kebab.enums.OrderStatus;
+import com.p4zd4n.kebab.enums.OrderType;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Order extends WithTimestamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "order_date_time", nullable = false)
+    private LocalDateTime orderDateTime;
+
+    @Column(name = "order_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OrderType orderType;
+
+    @Column(name = "order_status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @Column(name = "customer_phone", nullable = false)
+    private String customerPhone;
+
+    @Column(name = "street", nullable = false)
+    private String street;
+
+    @Column(name = "house_number", nullable = false)
+    private String houseNumber;
+
+    @Column(name = "postal_code", nullable = false)
+    private String postalCode;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+
+    @Column(name = "total_amount", nullable = false)
+    private BigDecimal totalAmount;
+
+    @Column(name = "is_active", nullable = false)
+    private boolean isActive;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems;
+
+    @Builder
+    public Order(LocalDateTime orderDateTime,
+                 OrderType orderType,
+                 OrderStatus orderStatus,
+                 String customerPhone,
+                 String street,
+                 String houseNumber,
+                 String postalCode,
+                 String city,
+                 BigDecimal totalAmount,
+                 boolean isActive) {
+        this.orderDateTime = orderDateTime;
+        this.orderType = orderType;
+        this.orderStatus = orderStatus;
+        this.customerPhone = customerPhone;
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.postalCode = postalCode;
+        this.city = city;
+        this.totalAmount = totalAmount;
+        this.isActive = isActive;
+    }
+}
