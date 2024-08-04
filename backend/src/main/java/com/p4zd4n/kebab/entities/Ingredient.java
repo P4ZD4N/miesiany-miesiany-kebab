8a91d7b15a3d438bc4b923351ba5372246127c1e
Wiktor <wiko700@gmail.com>
2024-08-04 20:34:00 +0200

    feat: add Ingredient entity

---
+++ backend/src/main/java/com/p4zd4n/kebab/entities/Ingredient.java
@@ -0,0 +1,28 @@
+package com.p4zd4n.kebab.entities;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "ingredients")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Ingredient extends WithTimestamp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Builder
+    public Ingredient(String name) {
+        this.name = name;
+    }
+}
