406e44a52e8acb36278defecce48f637e9dd93e3
Wiktor <wiko700@gmail.com>
2024-08-21 00:48:55 +0200

    feat: add hero section to home page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/common/navbar/navbar.component';
 
 import { TranslateModule } from '@ngx-translate/core';
+import { HeroSectionComponent } from './components/home/hero-section/hero-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, TranslateModule],
+  imports: [RouterOutlet, NavbarComponent, HeroSectionComponent, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ frontend/src/app/components/home/hero-section/hero-section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-hero-section',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './hero-section.component.html',
+  styleUrl: './hero-section.component.scss'
+})
+export class HeroSectionComponent {
+
+}
