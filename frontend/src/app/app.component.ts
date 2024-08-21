78864e7ca3dfbd899b3f0accb8f521f928592a75
Wiktor <wiko700@gmail.com>
2024-08-21 15:51:03 +0200

    feat: add about section to home page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from './components/common/navbar/navbar.component';
 
 import { TranslateModule } from '@ngx-translate/core';
 import { HeroSectionComponent } from './components/home/hero-section/hero-section.component';
+import { AboutSectionComponent } from './components/home/about-section/about-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeroSectionComponent, TranslateModule],
+  imports: [RouterOutlet, NavbarComponent, HeroSectionComponent, AboutSectionComponent, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
