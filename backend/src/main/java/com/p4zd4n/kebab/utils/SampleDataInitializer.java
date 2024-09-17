97f079e9c2de6d0f04d37340553815906e537078
Wiktor <wiko700@gmail.com>
2024-09-17 21:39:21 +0200

    feat: add SampleDataInitializer class and moved creating sample data logic there

---
+++ backend/src/main/java/com/p4zd4n/kebab/utils/SampleDataInitializer.java
@@ -0,0 +1,84 @@
+package com.p4zd4n.kebab.utils;
+
+import com.p4zd4n.kebab.entities.Employee;
+import com.p4zd4n.kebab.entities.Manager;
+import com.p4zd4n.kebab.entities.OpeningHour;
+import com.p4zd4n.kebab.enums.DayOfWeek;
+import com.p4zd4n.kebab.repositories.EmployeeRepository;
+import com.p4zd4n.kebab.repositories.OpeningHoursRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Arrays;
+
+@Component
+public class SampleDataInitializer implements CommandLineRunner {
+
+    private final EmployeeRepository employeeRepository;
+    private final OpeningHoursRepository openingHoursRepository;
+
+    public SampleDataInitializer(EmployeeRepository employeeRepository, OpeningHoursRepository openingHoursRepository) {
+        this.employeeRepository = employeeRepository;
+        this.openingHoursRepository = openingHoursRepository;
+    }
+
+    @Override
+    public void run(String... args) {
+
+        initOpeningHours();
+        initSampleEmployees();
+        initSampleManagers();
+    }
+
+    private void initOpeningHours() {
+
+        if (openingHoursRepository.count() == 0) {
+            Arrays.stream(DayOfWeek.values()).forEach(dayOfWeek -> {
+                OpeningHour openingHour = OpeningHour.builder()
+                        .dayOfWeek(dayOfWeek)
+                        .openingTime(LocalTime.of(10, 0))
+                        .closingTime(LocalTime.of(22, 0))
+                        .build();
+                openingHoursRepository.save(openingHour);
+            });
+        }
+    }
+
+    private void initSampleEmployees() {
+
+        Employee employee = Employee.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("employee@example.com")
+                .password(PasswordEncoder.encodePassword("employee123"))
+                .dateOfBirth(LocalDate.of(1990, 1, 1))
+                .phoneNumber("123456789")
+                .monthlySalary(BigDecimal.valueOf(3000))
+                .isActive(true)
+                .hiredAt(LocalDate.now())
+                .build();
+
+        employeeRepository.save(employee);
+    }
+
+    private void initSampleManagers() {
+
+        Manager manager = new Manager(
+                "Wiktor",
+                "Chudy",
+                "manager@example.com",
+                PasswordEncoder.encodePassword("manager123"),
+                LocalDate.of(2003, 8, 1),
+                "123456798",
+                BigDecimal.valueOf(5000),
+                true,
+                LocalDate.now()
+        );
+
+
+        employeeRepository.save(manager);
+    }
+}
