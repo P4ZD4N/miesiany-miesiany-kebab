17405112ff213f28159e2ee4635dd4e4870fbf9f
Wiktor <wiko700@gmail.com>
2025-01-27 19:12:28 +0100

    feat: add unit tests for addMealPromotion() method from PromotionsController

--- backend/src/test/java/com/p4zd4n/kebab/controllers/PromotionsControllerTest.java
+++ backend/src/test/java/com/p4zd4n/kebab/controllers/PromotionsControllerTest.java
@@ -1,7 +1,11 @@
 package com.p4zd4n.kebab.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.p4zd4n.kebab.enums.Size;
+import com.p4zd4n.kebab.requests.menu.beverages.NewBeverageRequest;
+import com.p4zd4n.kebab.requests.promotions.mealpromotions.NewMealPromotionRequest;
 import com.p4zd4n.kebab.responses.promotions.mealpromotions.MealPromotionResponse;
+import com.p4zd4n.kebab.responses.promotions.mealpromotions.NewMealPromotionResponse;
 import com.p4zd4n.kebab.services.promotions.MealPromotionsService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -21,12 +27,16 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(PromotionsController.class)
 @AutoConfigureMockMvc(addFilters = false)
 public class PromotionsControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Autowired
     private MockMvc mockMvc;
@@ -68,5 +78,76 @@ public class PromotionsControllerTest {
                 .andExpect(jsonPath("$[1].description", is("Small and medium -10%")));
 
         verify(mealPromotionsService, times(1)).getMealPromotions();
+    }
+
+    @Test
+    public void addMealPromotion_ShouldReturnOk_WhenValidRequest() throws Exception {
+
+        NewMealPromotionRequest request = NewMealPromotionRequest.builder()
+                .newMealPromotionDescription("-10%")
+                .newMealPromotionSizes(Set.of(Size.SMALL))
+                .newMealPromotionDiscountPercentage(BigDecimal.valueOf(10))
+                .newMealPromotionMealNames(List.of("Pita"))
+                .build();
+
+        NewMealPromotionResponse response = NewMealPromotionResponse.builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("Successfully added new meal promotion with id '1'")
+                .build();
+
+        when(mealPromotionsService.addMealPromotion(request)).thenReturn(response);
+
+        mockMvc.perform(post("/api/v1/promotions/add-meal-promotion")
+                .header("Accept-Language", "en")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status_code", is(HttpStatus.OK.value())))
+                .andExpect(jsonPath("$.message", is("Successfully added new meal promotion with id '1'")));
+    }
+
+    @Test
+    public void addMealPromotion_ShouldReturnBadRequest_WhenInvalidDescription() throws Exception {
+
+        NewMealPromotionRequest request = NewMealPromotionRequest.builder()
+                .newMealPromotionDescription("")
+                .newMealPromotionSizes(Set.of(Size.SMALL))
+                .newMealPromotionDiscountPercentage(BigDecimal.valueOf(10))
+                .newMealPromotionMealNames(List.of("Pita"))
+                .build();
+
+        mockMvc.perform(post("/api/v1/promotions/add-meal-promotion")
+                .header("Accept-Language", "en")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void addMealPromotion_ShouldReturnBadRequest_WhenMissingHeader() throws Exception {
+
+        NewMealPromotionRequest request = NewMealPromotionRequest.builder()
+                .newMealPromotionDescription("-10%")
+                .newMealPromotionSizes(Set.of(Size.SMALL))
+                .newMealPromotionDiscountPercentage(BigDecimal.valueOf(10))
+                .newMealPromotionMealNames(List.of("Pita"))
+                .build();
+
+        mockMvc.perform(post("/api/v1/promotions/add-meal-promotion")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void addMealPromotion_ShouldReturnBadRequest_WhenInvalidHeader() throws Exception {
+
+        String[] invalidHeaders = {"fr", "ES", "ENG", "RuS", "GER", "Sw", "aa", ""};
+
+        for (String header : invalidHeaders) {
+            mockMvc.perform(post("/api/v1/promotions/add-meal-promotion")
+                    .header("Accept-Language", header))
+                    .andExpect(status().isBadRequest());
+        }
     }
 }
