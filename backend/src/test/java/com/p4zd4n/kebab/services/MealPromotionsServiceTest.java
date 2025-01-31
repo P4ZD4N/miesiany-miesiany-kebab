50355a90106fce3ad603735b8208eaa17edf667c
Wiktor <wiko700@gmail.com>
2025-01-31 16:35:03 +0100

    feat: add unit tests for findMealPromotionById() and updateMealPromotion() methods from MealPromotionsService

--- backend/src/test/java/com/p4zd4n/kebab/services/MealPromotionsServiceTest.java
+++ backend/src/test/java/com/p4zd4n/kebab/services/MealPromotionsServiceTest.java
@@ -2,11 +2,14 @@ package com.p4zd4n.kebab.services;
 
 import com.p4zd4n.kebab.entities.MealPromotion;
 import com.p4zd4n.kebab.enums.Size;
+import com.p4zd4n.kebab.exceptions.notfound.MealPromotionNotFoundException;
 import com.p4zd4n.kebab.repositories.MealPromotionsRepository;
 import com.p4zd4n.kebab.repositories.MealRepository;
 import com.p4zd4n.kebab.requests.promotions.mealpromotions.NewMealPromotionRequest;
+import com.p4zd4n.kebab.requests.promotions.mealpromotions.UpdatedMealPromotionRequest;
 import com.p4zd4n.kebab.responses.promotions.mealpromotions.MealPromotionResponse;
 import com.p4zd4n.kebab.responses.promotions.mealpromotions.NewMealPromotionResponse;
+import com.p4zd4n.kebab.responses.promotions.mealpromotions.UpdatedMealPromotionResponse;
 import com.p4zd4n.kebab.services.promotions.MealPromotionsService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,10 +21,10 @@ import org.springframework.http.HttpStatus;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class MealPromotionsServiceTest {
@@ -94,5 +97,67 @@ public class MealPromotionsServiceTest {
         assertEquals("Successfully added new meal promotion with id '1'", response.message());
 
         verify(mealPromotionsRepository, times(1)).save(any(MealPromotion.class));
+    }
+
+    @Test
+    public void findMealPromotionById_ShouldReturnMealPromotion_IfExists() {
+
+        MealPromotion mealPromotion = MealPromotion.builder()
+                .description("Large -20%")
+                .sizes(Set.of(Size.LARGE))
+                .discountPercentage(BigDecimal.valueOf(20))
+                .build();
+
+        when(mealPromotionsRepository.findById(1L)).thenReturn(Optional.of(mealPromotion));
+
+        MealPromotion foundMealPromotion = mealPromotionsService.findMealPromotionById(1L);
+
+        assertNotNull(foundMealPromotion);
+        assertEquals("Large -20%", foundMealPromotion.getDescription());
+        assertEquals(1, foundMealPromotion.getSizes().size());
+
+        verify(mealPromotionsRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    public void findMealPromotionById_ShouldThrowMealPromotionsNotFoundException_IfDoesNotExists() {
+
+        when(mealPromotionsRepository.findById(100L)).thenThrow(new MealPromotionNotFoundException(100L));
+
+        MealPromotionNotFoundException exception = assertThrows(MealPromotionNotFoundException.class, () -> {
+            mealPromotionsService.findMealPromotionById(100L);
+        });
+
+        assertEquals("Meal promotion with id '100' not found!", exception.getMessage());
+
+        verify(mealPromotionsRepository, times(1)).findById(100L);
+    }
+
+    @Test
+    public void updateMealPromotion_ShouldUpdateMealPromotion_WhenValidRequest() {
+
+        MealPromotion mealPromotion = MealPromotion.builder()
+                .description("Large -20%")
+                .sizes(Set.of(Size.LARGE))
+                .discountPercentage(BigDecimal.valueOf(20))
+                .build();
+        mealPromotion.setId(1L);
+
+        UpdatedMealPromotionRequest request = UpdatedMealPromotionRequest.builder()
+                .updatedMealPromotionId(1L)
+                .updatedMealPromotionDescription("Siema")
+                .build();
+
+        when(mealPromotionsRepository.findById(request.updatedMealPromotionId())).thenReturn(Optional.of(mealPromotion));
+        when(mealPromotionsRepository.save(any(MealPromotion.class))).thenReturn(mealPromotion);
+
+        UpdatedMealPromotionResponse response = mealPromotionsService.updateMealPromotion(mealPromotion, request);
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK.value(), response.statusCode());
+        assertEquals("Successfully updated meal promotion with id '1'", response.message());
+
+        verify(mealPromotionsRepository, times(1)).findById(1L);
+        verify(mealPromotionsRepository, times(1)).save(mealPromotion);
     }
 }
