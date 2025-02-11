5fe375142aaea56c99b5b1373bcdc93fd1b8abe1
Wiktor <wiko700@gmail.com>
2025-02-11 16:33:12 +0100

    feat: add @Builder annotation

--- backend/src/main/java/com/p4zd4n/kebab/requests/promotions/beveragepromotions/RemovedBeveragePromotionRequest.java
+++ backend/src/main/java/com/p4zd4n/kebab/requests/promotions/beveragepromotions/RemovedBeveragePromotionRequest.java
@@ -1,7 +1,9 @@
 package com.p4zd4n.kebab.requests.promotions.beveragepromotions;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 
+@Builder
 public record RemovedBeveragePromotionRequest(
 
         @NotNull(message = "{id.notNull}")
