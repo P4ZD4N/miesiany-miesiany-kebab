2043bc36dd5ea57d2a60d1da463e371baa4b1b88
Wiktor <wiko700@gmail.com>
2025-02-11 14:26:31 +0100

    feat: add unit tests for addBeveragePromotion() method from BeveragePromotionsService

--- backend/src/test/java/com/p4zd4n/kebab/services/BeveragePromotionsServiceTest.java
+++ backend/src/test/java/com/p4zd4n/kebab/services/BeveragePromotionsServiceTest.java
@@ -5,14 +5,19 @@ import com.p4zd4n.kebab.entities.MealPromotion;
 import com.p4zd4n.kebab.enums.Size;
 import com.p4zd4n.kebab.repositories.BeveragePromotionsRepository;
 import com.p4zd4n.kebab.repositories.BeverageRepository;
+import com.p4zd4n.kebab.requests.promotions.beveragepromotions.NewBeveragePromotionRequest;
+import com.p4zd4n.kebab.requests.promotions.mealpromotions.NewMealPromotionRequest;
 import com.p4zd4n.kebab.responses.promotions.beveragepromotions.BeveragePromotionResponse;
+import com.p4zd4n.kebab.responses.promotions.beveragepromotions.NewBeveragePromotionResponse;
 import com.p4zd4n.kebab.responses.promotions.mealpromotions.MealPromotionResponse;
+import com.p4zd4n.kebab.responses.promotions.mealpromotions.NewMealPromotionResponse;
 import com.p4zd4n.kebab.services.promotions.BeveragePromotionsService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
@@ -21,6 +26,7 @@ import java.util.Map;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 public class BeveragePromotionsServiceTest {
@@ -62,5 +68,58 @@ public class BeveragePromotionsServiceTest {
         assertEquals(BigDecimal.valueOf(10), result.getLast().discountPercentage());
 
         verify(beveragePromotionsRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void addBeveragePromotion_ShouldAddBeverage_WhenValidRequestWithNullMapInRequest() {
+
+        NewBeveragePromotionRequest request = NewBeveragePromotionRequest.builder()
+                .description("-10%")
+                .discountPercentage(BigDecimal.valueOf(10))
+                .build();
+
+        BeveragePromotion newBeveragePromotion = BeveragePromotion.builder()
+                .description(request.description())
+                .discountPercentage(request.discountPercentage())
+                .build();
+        newBeveragePromotion.setId(1L);
+
+        when(beveragePromotionsRepository.save(any(BeveragePromotion.class))).thenReturn(newBeveragePromotion);
+
+        NewBeveragePromotionResponse response = beveragePromotionsService.addBeveragePromotion(request);
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK.value(), response.statusCode());
+        assertEquals("Successfully added new beverage promotion with id '1'", response.message());
+
+        verify(beveragePromotionsRepository, times(1)).save(any(BeveragePromotion.class));
+    }
+
+    @Test
+    public void addBeveragePromotion_ShouldAddBeverage_WhenValidRequestWithNotNullMapInRequest() {
+
+        NewBeveragePromotionRequest request = NewBeveragePromotionRequest.builder()
+                .description("-10%")
+                .discountPercentage(BigDecimal.valueOf(10))
+                .beveragesWithCapacities(Map.of(
+                        "Coca-Cola", List.of(BigDecimal.valueOf(0.5))
+                ))
+                .build();
+
+        BeveragePromotion newBeveragePromotion = BeveragePromotion.builder()
+                .description(request.description())
+                .discountPercentage(request.discountPercentage())
+                .build();
+        newBeveragePromotion.setId(1L);
+
+        when(beveragePromotionsRepository.save(any(BeveragePromotion.class))).thenReturn(newBeveragePromotion);
+
+        NewBeveragePromotionResponse response = beveragePromotionsService.addBeveragePromotion(request);
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK.value(), response.statusCode());
+        assertEquals("Successfully added new beverage promotion with id '1'", response.message());
+
+        verify(beveragePromotionsRepository, times(1)).save(any(BeveragePromotion.class));
     }
 }
