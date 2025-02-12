d5c67c0cf1f9dde68f0709d6caf292e515f279a1
Wiktor <wiko700@gmail.com>
2025-02-12 20:21:10 +0100

    feat: add unit tests for findAddonPromotionById() and updateAddonPromotion() methods from AddonPromotionsService

--- backend/src/test/java/com/p4zd4n/kebab/services/AddonPromotionsServiceTest.java
+++ backend/src/test/java/com/p4zd4n/kebab/services/AddonPromotionsServiceTest.java
@@ -2,13 +2,19 @@ package com.p4zd4n.kebab.services;
 
 import com.p4zd4n.kebab.entities.AddonPromotion;
 import com.p4zd4n.kebab.entities.BeveragePromotion;
+import com.p4zd4n.kebab.exceptions.notfound.AddonPromotionNotFoundException;
+import com.p4zd4n.kebab.exceptions.notfound.BeveragePromotionNotFoundException;
 import com.p4zd4n.kebab.repositories.AddonPromotionsRepository;
 import com.p4zd4n.kebab.repositories.AddonRepository;
 import com.p4zd4n.kebab.requests.promotions.addonpromotions.NewAddonPromotionRequest;
+import com.p4zd4n.kebab.requests.promotions.addonpromotions.UpdatedAddonPromotionRequest;
 import com.p4zd4n.kebab.requests.promotions.beveragepromotions.NewBeveragePromotionRequest;
+import com.p4zd4n.kebab.requests.promotions.beveragepromotions.UpdatedBeveragePromotionRequest;
 import com.p4zd4n.kebab.responses.promotions.addonpromotions.AddonPromotionResponse;
 import com.p4zd4n.kebab.responses.promotions.addonpromotions.NewAddonPromotionResponse;
+import com.p4zd4n.kebab.responses.promotions.addonpromotions.UpdatedAddonPromotionResponse;
 import com.p4zd4n.kebab.responses.promotions.beveragepromotions.NewBeveragePromotionResponse;
+import com.p4zd4n.kebab.responses.promotions.beveragepromotions.UpdatedBeveragePromotionResponse;
 import com.p4zd4n.kebab.services.promotions.AddonPromotionsService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,13 +24,9 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 public class AddonPromotionsServiceTest {
@@ -117,5 +119,64 @@ public class AddonPromotionsServiceTest {
         assertEquals("Successfully added new addon promotion with id '1'", response.message());
 
         verify(addonPromotionsRepository, times(1)).save(any(AddonPromotion.class));
+    }
+
+    @Test
+    public void findAddonPromotionById_ShouldReturnAddonPromotion_IfExists() {
+
+        AddonPromotion addonPromotion = AddonPromotion.builder()
+                .description("-20%")
+                .discountPercentage(BigDecimal.valueOf(20))
+                .build();
+
+        when(addonPromotionsRepository.findById(1L)).thenReturn(Optional.of(addonPromotion));
+
+        AddonPromotion foundAddonPromotion = addonPromotionsService.findAddonPromotionById(1L);
+
+        assertNotNull(foundAddonPromotion);
+        assertEquals("-20%", foundAddonPromotion.getDescription());
+        assertEquals(BigDecimal.valueOf(20), foundAddonPromotion.getDiscountPercentage());
+
+        verify(addonPromotionsRepository, times(1)).findById(1L);
+    }
+
+    @Test
+    public void findAddonPromotionById_ShouldThrowAddonPromotionNotFoundException_IfDoesNotExists() {
+
+        when(addonPromotionsRepository.findById(100L)).thenThrow(new AddonPromotionNotFoundException(100L));
+
+        AddonPromotionNotFoundException exception = assertThrows(AddonPromotionNotFoundException.class, () -> {
+            addonPromotionsService.findAddonPromotionById(100L);
+        });
+
+        assertEquals("Addon promotion with id '100' not found!", exception.getMessage());
+
+        verify(addonPromotionsRepository, times(1)).findById(100L);
+    }
+
+    @Test
+    public void updateAddonPromotion_ShouldUpdateAddonPromotion_WhenValidRequest() {
+
+        AddonPromotion addonPromotion = AddonPromotion.builder()
+                .description("-20%")
+                .discountPercentage(BigDecimal.valueOf(20))
+                .build();
+        addonPromotion.setId(1L);
+
+        UpdatedAddonPromotionRequest request = UpdatedAddonPromotionRequest.builder()
+                .id(1L)
+                .updatedDescription("Siema")
+                .build();
+
+        when(addonPromotionsRepository.save(any(AddonPromotion.class))).thenReturn(addonPromotion);
+
+        UpdatedAddonPromotionResponse response = addonPromotionsService.updateAddonPromotion(addonPromotion, request);
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK.value(), response.statusCode());
+        assertEquals("Successfully updated addon promotion with id '1'", response.message());
+        assertEquals("Siema", addonPromotion.getDescription());
+
+        verify(addonPromotionsRepository, times(1)).save(addonPromotion);
     }
 }
