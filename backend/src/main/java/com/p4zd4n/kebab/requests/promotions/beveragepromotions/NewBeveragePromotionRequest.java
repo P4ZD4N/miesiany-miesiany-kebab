ccb7a1284c523a563815e2281aaa7890644aad6e
Wiktor <wiko700@gmail.com>
2025-02-18 17:00:51 +0100

    feat: add validation for discount percentage field in NewBeveragePromotionRequest

--- backend/src/main/java/com/p4zd4n/kebab/requests/promotions/beveragepromotions/NewBeveragePromotionRequest.java
+++ backend/src/main/java/com/p4zd4n/kebab/requests/promotions/beveragepromotions/NewBeveragePromotionRequest.java
@@ -1,6 +1,8 @@
 package com.p4zd4n.kebab.requests.promotions.beveragepromotions;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -17,6 +19,8 @@ public record NewBeveragePromotionRequest(
 
         @JsonProperty("discount_percentage")
         @NotNull(message = "{discountPercentage.notNull}")
+        @DecimalMin(value = "0.0", message = "{discountPercentage.min}")
+        @DecimalMax(value = "100.0", message = "{discountPercentage.max}")
         BigDecimal discountPercentage,
 
         @JsonProperty("beverages_with_capacities")
