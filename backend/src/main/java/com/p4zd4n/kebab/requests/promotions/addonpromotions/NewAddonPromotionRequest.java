f0dd3724c98167d9e2b275564efd8d2f97b3cf73
Wiktor <wiko700@gmail.com>
2025-02-19 09:24:15 +0100

    feat: add validation annotations to ensure proper discount percentage between 0 and 100%

--- backend/src/main/java/com/p4zd4n/kebab/requests/promotions/addonpromotions/NewAddonPromotionRequest.java
+++ backend/src/main/java/com/p4zd4n/kebab/requests/promotions/addonpromotions/NewAddonPromotionRequest.java
@@ -1,6 +1,8 @@
 package com.p4zd4n.kebab.requests.promotions.addonpromotions;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -16,6 +18,8 @@ public record NewAddonPromotionRequest(
 
         @JsonProperty("discount_percentage")
         @NotNull(message = "{discountPercentage.notNull}")
+        @DecimalMin(value = "0.0", message = "{discountPercentage.min}")
+        @DecimalMax(value = "100.0", message = "{discountPercentage.max}")
         BigDecimal discountPercentage,
 
         @JsonProperty("addon_names")
