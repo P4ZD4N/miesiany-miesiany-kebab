778bfbf976ffc834f63f86bac310a4014c84db4f
Wiktor <wiko700@gmail.com>
2025-02-25 23:59:57 +0100

    feat: add /verify-subscription and /regenerate-otp endpoints

--- backend/src/main/java/com/p4zd4n/kebab/controllers/NewsletterController.java
+++ backend/src/main/java/com/p4zd4n/kebab/controllers/NewsletterController.java
@@ -2,10 +2,14 @@ package com.p4zd4n.kebab.controllers;
 
 import com.p4zd4n.kebab.exceptions.invalid.InvalidAcceptLanguageHeaderValue;
 import com.p4zd4n.kebab.requests.newsletter.NewNewsletterSubscriberRequest;
+import com.p4zd4n.kebab.requests.newsletter.RegenerateOtpRequest;
+import com.p4zd4n.kebab.requests.newsletter.VerifyNewsletterSubscriptionRequest;
 import com.p4zd4n.kebab.responses.newsletter.NewNewsletterSubscriberResponse;
 import com.p4zd4n.kebab.responses.newsletter.NewsletterSubscriberResponse;
-import com.p4zd4n.kebab.responses.promotions.mealpromotions.MealPromotionResponse;
+import com.p4zd4n.kebab.responses.newsletter.RegenerateOtpResponse;
+import com.p4zd4n.kebab.responses.newsletter.VerifyNewsletterSubscriptionResponse;
 import com.p4zd4n.kebab.services.newsletter.NewsletterService;
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -35,7 +39,7 @@ public class NewsletterController {
     public ResponseEntity<NewNewsletterSubscriberResponse> subscribe(
             @RequestHeader(value = "Accept-Language") String language,
             @Valid @RequestBody NewNewsletterSubscriberRequest request
-    ) {
+    ) throws MessagingException {
         if (!language.equalsIgnoreCase("en") && !language.equalsIgnoreCase("pl")) {
             throw new InvalidAcceptLanguageHeaderValue(language);
         }
@@ -45,6 +49,42 @@ public class NewsletterController {
         NewNewsletterSubscriberResponse response = newsletterService.addNewsletterSubscriber(request);
 
         log.info("Successfully added new newsletter subscriber");
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/verify-subscription")
+    public ResponseEntity<VerifyNewsletterSubscriptionResponse> verifySubscription(
+            @RequestHeader(value = "Accept-Language") String language,
+            @Valid @RequestBody VerifyNewsletterSubscriptionRequest request
+    ) throws MessagingException {
+        if (!language.equalsIgnoreCase("en") && !language.equalsIgnoreCase("pl")) {
+            throw new InvalidAcceptLanguageHeaderValue(language);
+        }
+
+        log.info("Received verify subscription request");
+
+        VerifyNewsletterSubscriptionResponse response = newsletterService.verifySubscription(request);
+
+        log.info("Successfully verified newsletter subscriber");
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/regenerate-otp")
+    public ResponseEntity<RegenerateOtpResponse> regenerateOtp(
+            @RequestHeader(value = "Accept-Language") String language,
+            @Valid @RequestBody RegenerateOtpRequest request
+    ) throws MessagingException {
+        if (!language.equalsIgnoreCase("en") && !language.equalsIgnoreCase("pl")) {
+            throw new InvalidAcceptLanguageHeaderValue(language);
+        }
+
+        log.info("Received regenerate otp request");
+
+        RegenerateOtpResponse response = newsletterService.regenerateOtp(request);
+
+        log.info("Successfully regenerated otp");
 
         return ResponseEntity.ok(response);
     }
