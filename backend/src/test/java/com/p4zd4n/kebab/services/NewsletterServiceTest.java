c73376f3a98d42951fd66404d3939942a8fd6c13
Wiktor <wiko700@gmail.com>
2025-02-25 18:47:16 +0100

    feat: add unit tests for addNewsletterSubscriber() method from NewsletterService

--- backend/src/test/java/com/p4zd4n/kebab/services/NewsletterServiceTest.java
+++ backend/src/test/java/com/p4zd4n/kebab/services/NewsletterServiceTest.java
@@ -1,8 +1,10 @@
 package com.p4zd4n.kebab.services;
 
 import com.p4zd4n.kebab.entities.NewsletterSubscriber;
-import com.p4zd4n.kebab.enums.Size;
+import com.p4zd4n.kebab.exceptions.alreadyexists.SubscriberAlreadyExistsException;
 import com.p4zd4n.kebab.repositories.NewsletterRepository;
+import com.p4zd4n.kebab.requests.newsletter.NewNewsletterSubscriberRequest;
+import com.p4zd4n.kebab.responses.newsletter.NewNewsletterSubscriberResponse;
 import com.p4zd4n.kebab.responses.newsletter.NewsletterSubscriberResponse;
 import com.p4zd4n.kebab.services.newsletter.NewsletterService;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,11 +12,13 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
 
-import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class NewsletterServiceTest {
@@ -59,5 +63,56 @@ public class NewsletterServiceTest {
         assertEquals(true, result.getLast().isActive());
 
         verify(newsletterRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void addNewsletterSubscriber_ShouldAddSubscriber_WhenSubscriberWithTheSameEmailDoesNotExist() {
+
+        NewNewsletterSubscriberRequest request = NewNewsletterSubscriberRequest.builder()
+                .firstName("Wiktor")
+                .email("example@example.com")
+                .build();
+
+        when(newsletterRepository.findByEmail(request.email())).thenReturn(Optional.empty());
+
+        NewsletterSubscriber newSubscriber = NewsletterSubscriber.builder()
+                .subscriberFirstName(request.firstName())
+                .email(request.email())
+                .isActive(false)
+                .build();
+
+        when(newsletterRepository.save(any(NewsletterSubscriber.class))).thenReturn(newSubscriber);
+
+        NewNewsletterSubscriberResponse response = newsletterService.addNewsletterSubscriber(request);
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK.value(), response.statusCode());
+        assertEquals("Successfully added new newsletter subscriber with email 'example@example.com'", response.message());
+
+        verify(newsletterRepository, times(1)).findByEmail(request.email());
+        verify(newsletterRepository, times(1)).save(any(NewsletterSubscriber.class));
+    }
+
+    @Test
+    public void addNewsletterSubscriber_ShouldThrowSubscriberAlreadyExistsException_WhenSubscriberWithTheSameEmailExists() {
+
+        NewNewsletterSubscriberRequest request = NewNewsletterSubscriberRequest.builder()
+                .firstName("Wiktor")
+                .email("example@example.com")
+                .build();
+
+        NewsletterSubscriber existingSubscriber = NewsletterSubscriber.builder()
+                .subscriberFirstName("Michael")
+                .email(request.email())
+                .isActive(false)
+                .build();
+
+        when(newsletterRepository.findByEmail(request.email())).thenReturn(Optional.of(existingSubscriber));
+
+        assertThrows(SubscriberAlreadyExistsException.class, () -> {
+            newsletterService.addNewsletterSubscriber(request);
+        });
+
+        verify(newsletterRepository, times(1)).findByEmail(request.email());
     }
 }
