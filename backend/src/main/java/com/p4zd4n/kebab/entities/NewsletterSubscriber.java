35442b75afada52e855307d7cccdb25461340c6b
Wiktor <wiko700@gmail.com>
2025-02-25 23:54:28 +0100

    feat: add necessary fields to make verification emails working and change constructor

--- backend/src/main/java/com/p4zd4n/kebab/entities/NewsletterSubscriber.java
+++ backend/src/main/java/com/p4zd4n/kebab/entities/NewsletterSubscriber.java
@@ -1,10 +1,13 @@
 package com.p4zd4n.kebab.entities;
 
+import com.p4zd4n.kebab.enums.NewsletterMessagesLanguage;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "newsletter_subscribers")
@@ -24,13 +27,32 @@ public class NewsletterSubscriber extends WithTimestamp {
     @Column(name = "email", nullable = false, unique = true)
     private String email;
 
+    @Column(name = "newsletter_language", nullable = false)
+    private NewsletterMessagesLanguage newsletterMessagesLanguage;
+
     @Column(name = "is_active", nullable = false)
     private boolean isActive;
 
+    @Column(name = "otp")
+    private Integer otp;
+
+    @Column(name = "otp_generated_time")
+    private LocalDateTime otpGeneratedTime;
+
     @Builder
-    public NewsletterSubscriber(String subscriberFirstName, String email, boolean isActive) {
+    public NewsletterSubscriber(
+            String subscriberFirstName,
+            String email,
+            NewsletterMessagesLanguage newsletterMessagesLanguage,
+            boolean isActive,
+            Integer otp,
+            LocalDateTime otpGeneratedTime
+    ) {
         this.subscriberFirstName = subscriberFirstName;
         this.email = email;
+        this.newsletterMessagesLanguage = newsletterMessagesLanguage;
         this.isActive = isActive;
+        this.otp = otp;
+        this.otpGeneratedTime = otpGeneratedTime;
     }
 }
