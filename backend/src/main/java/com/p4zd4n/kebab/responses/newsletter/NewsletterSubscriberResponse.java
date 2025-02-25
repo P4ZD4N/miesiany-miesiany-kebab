747e4b3995493479186c1a7918052404b614328f
Wiktor <wiko700@gmail.com>
2025-02-25 23:58:29 +0100

    feat: add newsletterMessagesLanguage property

--- backend/src/main/java/com/p4zd4n/kebab/responses/newsletter/NewsletterSubscriberResponse.java
+++ backend/src/main/java/com/p4zd4n/kebab/responses/newsletter/NewsletterSubscriberResponse.java
@@ -1,6 +1,7 @@
 package com.p4zd4n.kebab.responses.newsletter;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.p4zd4n.kebab.enums.NewsletterMessagesLanguage;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 public record NewsletterSubscriberResponse(
         String email,
         @JsonProperty("first_name") String firstName,
+        @JsonProperty("messages_language") NewsletterMessagesLanguage newsletterMessagesLanguage,
         @JsonProperty("subscribed_at") LocalDateTime subscribedAt,
         @JsonProperty("is_active") Boolean isActive
 ) {}
